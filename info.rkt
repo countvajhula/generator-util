69bab6a7d194b601b093b0cc0639bcb9bec374f9
Siddhartha <sid@countvajhula.com>
2020-08-19 20:04:14 -0700

    add coverage checking, coveralls

--- info.rkt
+++ info.rkt
@@ -8,9 +8,11 @@
                      "racket-doc"
                      "rackunit-lib"
                      "sandbox-lib"
+                     "cover"
+                     "cover-coveralls"
                      "collections-doc"))
 (define scribblings '(("scribblings/generator-util.scrbl" ())))
-(define compile-omit-paths '("dev"))
+(define compile-omit-paths '("dev" "coverage"))
 (define clean '("compiled" "doc"))
 (define pkg-desc "Useful utilities for working with generators")
 (define version "1.1")
